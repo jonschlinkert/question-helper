55411ccc8ad174fcefad5e29363b2c860a407cef
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-07 03:13:02 -0400

    update tests

--- test.js
+++ test.js
@@ -1,154 +1,143 @@
 'use strict';
 
-var engineHandlebars = require('engine-handlebars');
-var Template = require('template');
+var assert = require('assert');
 var should = require('should');
-
+var Template = require('template');
 var question = require('./');
-var timeout = 5000;
 
+var timeout = 5000;
 var context = {
   questions: {
-    name: "What's your name?",
-    dob: "When's your birtday?"
+    name: 'What\'s your name?',
+    dob: 'When\'s your birtday?'
   }
 };
 
-describe('question', function () {
-  describe('as a function', function () {
-    it('should return an answer given a question', function (done) {
-      this.timeout(timeout);
-      question("What's your name?", function (err, answer) {
-        answer.should.exist;
-        done();
-      });
-    });
-    it('should return an answer given a question key', function (done) {
-      this.timeout(timeout);
-      
-      question.call({ctx: context}, 'name', function (err, answer) {
-        answer.should.exist;
-        done();
-      });
-    });
-    it('should throw an error when a key is not passed in', function (done) {
-      this.timeout(timeout);
-      try {
-        question(function (err, answer) {
-          if (err) return done();
-          done(new Error('Expected an error to be thrown'));
-        });
-      } catch (err) {
-        done();
-      }
-    });
-    it('should throw an error when a key is not a string', function (done) {
-      question({}, function (err, answer) {
-        if (err) {
-          err.message.indexOf('Question expected the first parameter to be a string').should.equal(0);
-          return done();
-        }
-        done(new Error('Expected an error to be thrown'));
-      });
+describe('questions', function () {
+  it('should return an answer given a question', function (cb) {
+    this.timeout(timeout);
+    question('What\'s your name?', function (err, answer) {
+      answer.should.be.a.string;
+      cb();
     });
   });
 
-  describe('as a handlebars helper', function () {
-    var template = null;
-    beforeEach(function () {
-      template = new Template();
-      template.engine('hbs', engineHandlebars);
-      template.page('test-question.hbs', 'Your name is {{question "What\'s your name?"}}');
-      template.page('test-key.hbs', 'Your name is {{question "name"}}');
-      template.page('test-no-key.hbs', 'Your name is {{question}}');
-      template.page('test-bad-key.hbs', 'Your name is {{question questions}}');
-      template.asyncHelper('question', question);
-    });
-    it('should return an answer given a question', function (done) {
-      this.timeout(timeout);
-      template.render('test-question.hbs', function (err, content) {
-        if (err) console.log('err', err);
-        content.should.exist;
-        (content.indexOf('{{') === -1).should.be.true;
-        done();
-      });
-    });
-    it('should return an answer given a question key', function (done) {
-      this.timeout(timeout);      
-      template.render('test-key.hbs', context, function (err, content) {
-        if (err) console.log('err', err);
-        (content.indexOf('{{') === -1).should.be.true;
-        done();
-      });
-    });
-    it('should throw an error when a key is not passed in', function (done) {
-      this.timeout(timeout);
-      try {
-        template.render('test-no-key.hbs', context, function (err, content) {
-          if (err) return done();
-          done(new Error('Expected an error to be thrown'));
-        });
-      } catch (err) {
-        done();
-      }
-    });
-    it('should throw an error when a key is not a string', function (done) {
-      template.render('test-bad-key.hbs', context, function (err, content) {
-        if (err) {
-          err.message.indexOf('Question expected the first parameter to be a string').should.equal(0);
-          return done();
-        }
-        done(new Error('Expected an error to be thrown'));
-      });
+  it('should return an answer given a question key', function (cb) {
+    this.timeout(timeout);
+    question.call({ctx: context}, 'name', function (err, answer) {
+      answer.should.be.a.string;
+      cb();
     });
   });
 
-  describe('as a lodash helper', function () {
-    var template = null;
-    beforeEach(function () {
-      template = new Template();
-      template.page('test-question.html', 'Your name is <%= question("What\'s your name?") %>');
-      template.page('test-key.html', 'Your name is <%= question("name") %>');
-      template.page('test-no-key.html', 'Your name is <%= question() %>');
-      template.page('test-bad-key.html', 'Your name is <%= question(questions) %>');
-      template.asyncHelper('question', question);
+  it('should throw an error when a key is not passed in', function (cb) {
+    this.timeout(timeout);
+    question(function (err, answer) {
+      err.should.be.an.object;
+      err.should.have.property('message');
+      cb();
     });
-    it('should return an answer given a question', function (done) {
-      this.timeout(timeout);
-      template.render('test-question.html', function (err, content) {
-        if (err) console.log('err', err);
-        content.should.exist;
-        (content.indexOf('<%=') === -1).should.be.true;
-        done();
-      });
+  });
+  it('should throw an error when a key is not a string', function (cb) {
+    this.timeout(timeout);
+    question({}, function (err, answer) {
+      err.should.be.an.object;
+      err.should.have.property('message');
+      cb();
     });
-    it('should return an answer given a question key', function (done) {
-      this.timeout(timeout);      
-      template.render('test-key.html', context, function (err, content) {
-        if (err) console.log('err', err);
-        (content.indexOf('<%=') === -1).should.be.true;
-        done();
-      });
+  });
+});
+
+describe('as a handlebars helper', function () {
+  var template = null;
+  beforeEach(function () {
+    template = new Template();
+    template.engine('hbs', require('engine-handlebars'));
+    template.page('test-question.hbs', 'Your name is {{question "What\'s your name?"}}');
+    template.page('test-key.hbs', 'Your name is {{question "name"}}');
+    template.asyncHelper('question', question);
+  });
+  it('should return an answer given a question', function (cb) {
+    this.timeout(timeout);
+
+    template.render('test-question.hbs', function (err, content) {
+      if (err) console.log('err', err);
+      content.should.exist;
+      assert(content.indexOf('{{') === -1)
+      cb();
     });
-    it('should throw an error when a key is not passed in', function (done) {
-      this.timeout(timeout);
-      try {
-        template.render('test-no-key.html', context, function (err, content) {
-          if (err) return done();
-          done(new Error('Expected an error to be thrown'));
-        });
-      } catch (err) {
-        done();
-      }
+  });
+  it('should return an answer given a question key', function (cb) {
+    this.timeout(timeout);
+    template.render('test-key.hbs', context, function (err, content) {
+      if (err) console.log('err', err);
+      assert(content.indexOf('{{') === -1)
+      cb();
     });
-    it('should throw an error when a key is not a string', function (done) {
-      template.render('test-bad-key.html', context, function (err, content) {
-        if (err) {
-          err.message.indexOf('Question expected the first parameter to be a string').should.equal(0);
-          return done();
-        }
-        done(new Error('Expected an error to be thrown'));
-      });
+  });
+  it('should throw an error when a key is not passed in', function (cb) {
+    this.timeout(timeout);
+    template.render('', context, function (err, content) {
+      err.should.be.an.object;
+      err.should.have.property('message');
+      cb();
+    });
+  });
+  it('should throw an error when a key is not a string', function (cb) {
+    this.timeout(timeout);
+    template.render('test-bad-key.hbs', context, function (err, content) {
+      err.should.be.an.object;
+      err.should.have.property('message');
+      cb();
+    });
+  });
+});
+
+describe('lodash helper', function () {
+  var template = null;
+  beforeEach(function () {
+    template = new Template();
+    template.engine('html', require('engine-lodash'));
+    template.page('test-question.html', 'Your name is <%= question("What\'s your name?") %>');
+    template.page('test-key.html', 'Your name is <%= question("name") %>');
+    template.page('test-no-key.html', 'Your name is <%= question() %>');
+    template.asyncHelper('question', question);
+  });
+
+  it('should return an answer given a question', function (cb) {
+    this.timeout(timeout);
+    template.render('test-question.html', function (err, content) {
+      if (err) console.log('err', err);
+      content.should.exist;
+      assert(content.indexOf('<%=') === -1);
+      cb();
+    });
+  });
+
+  it('should return an answer given a question key', function (cb) {
+    this.timeout(timeout);
+    template.render('test-key.html', context, function (err, content) {
+      if (err) console.log('err', err);
+      assert(content.indexOf('<%=') === -1);
+      cb();
+    });
+  });
+
+  it('should throw an error when a key is not passed in', function (cb) {
+    this.timeout(timeout);
+    template.render('', context, function (err, content) {
+      err.should.be.an.object;
+      err.should.have.property('message');
+      cb();
+    });
+  });
+
+  it('should throw an error when a key is not a string', function (cb) {
+    this.timeout(timeout);
+    template.render('test-bad-key.html', context, function (err, content) {
+      err.should.be.an.object;
+      err.should.have.property('message');
+      cb();
     });
   });
 });
